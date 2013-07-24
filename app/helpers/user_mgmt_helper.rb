10a0b053acf25c315c4383ae8f1ec35da9be00ae
Fernando Gorodscy <fefow@me.com>
2013-07-24 18:22:43 -0400

    usage simplification

--- app/helpers/user_mgmt_helper.rb
+++ app/helpers/user_mgmt_helper.rb
@@ -7,22 +7,16 @@ module UserMgmtHelper
 			password_confirmation: password_confirmation }, method: :simple }, host
 	end
 
-	def sign_up uid, strategy, host='http://localhost:3000'
+	def sign_up_oauth uid, strategy, host='http://localhost:3000'
 		send_request :post, :users, { user: { uid: uid, strategy: strategy }, method: :simple }, host
 	end
 
-	def log_in id, code, method=:simple, host='http://localhost:3000'
-		if method == :simple
-			p1 = "email"
-			p2 = "password"
-		elsif method == :oauth
-			p1 = "uid"
-			p2 = "strategy"
-		else
-			raise ArgumentError.new 'Invalid method.'
-		end
+	def log_in email, password, host='http://localhost:3000'
+		send_request :get, :login, { user: { email: => email, password: => password }, method: :simple }, host
+	end
 
-		send_request :get, :login, { user: { p1 => id, p2 => code }, method: method }, host
+	def log_in_oauth uid, strategy, host='http://localhost:3000'
+		send_request :get, :login, { user: { uid: => uid, strategy: => strategy }, method: :oauth }, host
 	end
 
 	def log_out umid, host='http://localhost:3000'
@@ -49,18 +43,18 @@ module UserMgmtHelper
 									session: session }, host
 	end
 
-	def change_password reset_token, new_pw, new_pw_confirm, host='http://localhost:3000'
+	def reset_password reset_token, new_pw, new_pw_confirm, host='http://localhost:3000'
 		send_request :put, :change_password, { reset_token: reset_token, new_password: new_pw, 
 									new_password_confirmation: new_pw_confirm }, host
+	end
+
+	def reset_password_request email, host='http://localhost:3000'
+		send_request :post, :reset_password, { email: email }, host
 	end
 
 	def change_email user, password, new_email, new_email_confirm, session, host='http://localhost:3000'
 		send_request :put, :change_email, { email: user, password: password, new_email: new_email, 
 									new_email_confirmation: new_email_confirm, session: session }, host
-	end
-
-	def reset_password email, host='http://localhost:3000'
-		send_request :post, :reset_password, { email: email }, host
 	end
 
 	private
